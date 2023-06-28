ae9ad24ed43954b98260f0e4dd6a50b2778f3808
zhhhhhhhy <1301195487@qq.com>
2023-06-28 21:36:39 +0800

    实现了数据的显示

--- front-end/src/components/FileKeySearch.vue
+++ front-end/src/components/FileKeySearch.vue
@@ -1,48 +1,43 @@
 <template>
     <div>
-        <!-- <h1>{{ filename }}</h1>
-        <p>{{filetext}}</p> -->
         <div class="key-input">
-            <el-input placeholder="请输入你要搜索的关键词" v-model="input1"></el-input>
+            <el-input placeholder="请输入你要搜索的关键词" v-model="searchkey"></el-input>
         </div>
-        
-        <!-- <div>
-            <el-cascader :props="props" placeholder="选择你的文件夹地址" v-model="choose"></el-cascader>
-        </div> -->
 
         <div>
             <el-input placeholder="选中文件夹的url地址" v-model="input" :disabled="true"></el-input>
         </div>
 
         <div>
-            <el-table :data="tableData" stripe style="width: 100%" @row-click="tableItemClick">
-                <el-table-column prop="path" label="文件夹" width="180" ></el-table-column>
-                <el-table-column prop="chooseFolder" label="选择" width="180">
-                    <!-- <el-radio v-model="radio" :label="id">选择</el-radio> -->
-                    <button @click="tableButtonClick">选择</button>
-                </el-table-column>
-                <!-- <el-table-column prop="address" label="地址"></el-table-column> -->
-        </el-table>
-        </div> 
-        
-        <div>
             <button @click="getUrlFile()">提交</button>
         </div>
-        <!-- 展示从后端搜索出来的数据 -->
-        <div>
-            <li v-for="(user,i) in userlist" :key="user.id"> 索引是{{i}},{{user.username }}</li>    
-        </div>
 
+        <div>
+            <button @click="backToChoose()">返回上一级</button>
+        </div>
+        <!-- 展示从后端搜索出来的数据 -->
+        
+        <div class="ct">
+            <el-checkbox :indeterminate="isIndeterminate" v-model="checkAll" @change="handleCheckAllChange">全选</el-checkbox>
+            <div style="margin: 15px 0;"></div> 
+            <el-checkbox-group v-model="checkedFlies" @change="handleCheckedChange">
+                <div v-for="file in fileSearchList"  :key="file.id">
+                    <el-checkbox  :label="file.id">{{ file.filename }}</el-checkbox>
+                    <div v-for="data in file.content" :key="data.line">
+                        <span>{{ data.line }}</span>
+                        <span>{{ data.text }}</span>
+                    </div>
+                </div>
+                
+            </el-checkbox-group> 
+        </div> 
         
     </div>
 </template>
 
 <script>
     import axiox from "axios"
-
-
     var id = 0;
-    // var 
     var currentArray = []
     export default {
         created:function(){
@@ -70,106 +65,33 @@
         },
         data:function(){
             return{
-                flag:0,
-                radio: '0',
-                id:++id,
-                showlist:[],
+                checkAll: false,
+                isIndeterminate: false,
+                checkedFlies: [],
+                flag: 0,
+                id: ++id,
+                showlist: [],
                 tableData: [],
                 input: '',
-                input1:"",
-                userlist: [
-                    {id:1,username:'zhy',sex:"1"},
-                    {id:2,username:'zhy1',sex:"1"},
-                    {id:3,username:'zhy2',sex:"1"},
-                    {id:4,username:'zhy3',sex:"1"},
+                searchkey:"",
+                fileSearchList: [{
+                        id:1,
+                        filename:'zhy',
+                        content:[
+                            {line:"1",text:"123"},
+                            {line:"2",text:"123"},
+                            {line:"3",text:"123"},
+                    ]},
+                    {
+                        id:2,
+                        filename:'zhy',
+                        content:[
+                            {line:"1",text:"123"},
+                            {line:"2",text:"123"},
+                            {line:"3",text:"123"},
+                    ]}
                 ],
                 choose: "", 
-                props: {
-                    lazy: true,
-                    lazyLoad(node, resolve) {
-                        console.log(node)
-                        // const { level } = node;
-                        const level = node.level
-                        setTimeout(() => {
-                            async function getDir(){
-                                try{
-                                    // 判定如果是空的，那么需要获取盘符
-                                    // 将盘符末尾的冒号删去
-                                    console.log("我进入了这个函数")
-                                    console.log(currentArray.length === 0)
-                                    if(level === 0){
-                                        console.log("我进入了这个函数2")
-                                        const response = await axiox.get("http://localhost:8888/getDir?dir=getInit")
-                                        currentArray = response.data
-                                        for(var i=0;i<currentArray.length;i++){
-                                            currentArray[i] = currentArray[i].slice(0,currentArray[i].indexOf(":")+1)
-                                            console.log(currentArray[i])
-                                        }
-                                        const nodes = currentArray.map(item =>({
-                                            value:++id,
-                                            label:item,
-                                            leaf:level
-                                        }))
-                                        console.log(nodes)
-                                        resolve(nodes);
-                                    }
-                                    // 如果不是空的。就查看点击的位置，再服务器获取数据，添加到resolve中
-                                    else{
-                                        var path = [] 
-                                        // 获取node的level
-                                        // console.log(node.label)
-                                        // 它的祖宗们拼接，向服务器询问数据
-                                        // const response = await axiox.get("http://localhost:8888/getDir?dir=getInit")
-                                        // 将路径存入数组
-                                        
-                                        // 遍历寻找父亲的label添加到数组
-                                        for(var i=level;i>0;i--){
-                                            path.push(node.label)
-                                            node = node.parent
-                                        }
-                                        // 反向组成字符串保存为路径
-                                        var pathString = ""
-                                        for(var i=level-1;i>=0;i--){
-                                            pathString += path[i]
-                                            pathString += "/"
-                                        }
-                                        // pathString += path[0]
-                                        // 发送请求
-                                        console.log("我进入了else函数2")
-                                        console.log(node)
-                                        console.log(pathString)
-                                        const response = await axiox.get("http://localhost:8888/getDir?dir="+pathString)
-                                        currentArray = response.data
-                                        console.log("currentArray")
-                                        console.log(currentArray)
-                                        if(currentArray.length===0){
-                                            const nodes = currentArray.map(item =>({
-                                                value:++id,
-                                                label:item,
-                                                leaf:level
-                                            }))
-                                            resolve(nodes);
-                                        }
-                                        else{
-                                            const nodes = currentArray.map(item =>({
-                                                value:++id,
-                                                label:item,
-                                                leaf:level
-                                            }))
-                                            resolve(nodes);
-                                        }
-                                    }
-                                }
-                                catch(error){
-                                    console.error(error) 
-
-                                }
-                            }
-                            getDir()
-                        }, 1000);
-                    
-                    }
-                }
             }
         },
         methods:{
@@ -179,18 +101,6 @@
                 console.log("我触发了点击事件");
                 // 更新渲染表格
                 // 并展示
-            },
-            testClick:function(row,event,column){
-                console.log(row)
-                console.log(event)
-                console.log(column)
-                // alert("111")
-            },
-            testButton:function(e){
-                // alert()
-                console.log(111111111)
-                console.log(e)
-                console.log(111111111)
             },
             tableButtonClick:function(){
                 // table中的button的点击事件
@@ -227,11 +137,117 @@
                 }
                 else{
                     this.input+=row.path + "/"
+                    this.getDir(this.input)
                 }
                 this.flag = 0
                 // console.log("input:"+this.input)
                 // console.log("最后一个："+this.input.charAt(this.input.length - 1 ))
                 // console.log(this.input.charAt(this.input.length - 1 )=='/')
+            },
+            // 同步更改掉currentarray
+            getDir:async function(input){
+                try{
+                    const response = await axiox.get("http://localhost:8888/getDir?dir="+input)
+                    currentArray = response.data
+
+                    console.log("currentArray")
+                    console.log(currentArray)
+                    let td = []
+                    for(let i=0;i<currentArray.length;i++){
+                        let a = {}
+                        a.path = currentArray[i]
+                        td.push(a)
+                    }
+                    console.log(td)
+                    console.log(this.tableData)
+                    this.tableData = td
+                }
+                catch(error){
+                    console.error(error) 
+                }
+            },
+            submitButton:async function(){
+                try{
+                    // 等待后端的返回的端口已经方式
+                    const response = await axiox.get("http://localhost:8888/getDir?dir="+this.input+"&&key="+this.searchkey)
+                    currentArray = response.data
+                    // 从后端获取返回的数据
+                    
+                }
+                catch(error){
+                    console.error(error) 
+                }
+            },
+            backToChoose: async function(){
+                try{
+                    //去掉最后一个路径返回上一级
+                    // 1、去掉最后一个"/"
+                    this.input = this.input.slice(0,this.input.length-2)
+                    // console.log(input)
+                    //截取
+                    let index = this.input.lastIndexOf('/') 
+                    console.log(index)
+                    if(index == -1){
+                        this.input = ""
+                    }
+                    else {
+                        this.input = this.input.slice(0,index+1)
+                    }
+                    
+                    if(this.input==""){
+                        let response = await axiox.get("http://localhost:8888/getDir?dir=getInit")
+                        currentArray = response.data
+                        for(var i=0;i<currentArray.length;i++){
+                        currentArray[i] = currentArray[i].slice(0,currentArray[i].indexOf(":")+1)
+                        }
+                        console.log(111111111111111)
+                        console.log("currentArray")
+                        console.log(currentArray)
+                        var tableData = []
+                        for(let i=0;i<currentArray.length;i++){
+                            let a = {}
+                            a.path = currentArray[i]
+                            tableData.push(a)
+                        }
+                        console.log(tableData)
+                        console.log(this.tableData)
+                        this.tableData = tableData
+                    }
+                    else {
+                        let response = await axiox.get("http://localhost:8888/getDir?dir="+this.input)
+                        currentArray = response.data
+                        let td = []
+                        for(let i=0;i<currentArray.length;i++){
+                            let a = {}
+                            a.path = currentArray[i]
+                            td.push(a)
+                        }
+                        console.log(td)
+                        console.log(this.tableData)
+                        this.tableData = td
+                    }
+                }
+                catch(error){
+                    console.error(error) 
+                }
+            },
+            handleCheckAllChange(val) {
+                if(val){
+                    let a = new Array()
+                    for (let i=0;i<this.fileSearchList.length;i++){
+                        a.push(this.fileSearchList[i].id)
+                    }
+                    this.checkedFlies = a
+                }
+                else {
+                    this.checkedFlies = []
+                }
+                this.isIndeterminate = false;
+            },
+            handleCheckedChange(value) {
+                let checkedCount = value.length;
+                this.checkAll = checkedCount === this.fileSearchList.length;
+                this.isIndeterminate = checkedCount > 0 && checkedCount < this.fileSearchList.length;
             }
         }
     }
@@ -248,8 +264,19 @@
         margin: auto;
         margin-bottom: 10px;
     }
+    /* .showlist{
+        font-size: 10;
+    } */
+    /* .el-checkbox-group {
+        font-size:10
+    } */
 </style>
 
+<style scoped>
+    .ct >>> .el-checkbox-group {
+        font-size:20px 
+    }
+</style>
 
 
 
