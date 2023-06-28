701f896484987eb1d9db716c5186c4ca0b95bc48
zhhhhhhhy <1301195487@qq.com>
2023-06-29 07:22:38 +0800

    前端完成版本1

--- front-end/src/components/FileKeySearch.vue
+++ front-end/src/components/FileKeySearch.vue
@@ -8,29 +8,56 @@
             <el-input placeholder="选中文件夹的url地址" v-model="input" :disabled="true"></el-input>
         </div>
 
-        <div>
-            <button @click="getUrlFile()">提交</button>
+        <div class="file-choose-table">
+            <el-table :data="tableData" stripe style="width: 100%" @row-click="tableItemClick">
+                <el-table-column prop="path" label="文件夹" width="800" ></el-table-column>
+                <el-table-column prop="chooseFolder" label="选择" width="750">
+                    <!-- <el-radio v-model="radio" :label="id">选择</el-radio> -->
+                    <!-- <button @click="tableButtonClick">选择</button> -->
+                    <el-button type="primary" @click="tableButtonClick">选择</el-button>
+                    <!-- <el-button type="primary" :loading="true" @click="tableButtonClick">加载中</el-button> -->
+                </el-table-column>
+                <!-- <el-table-column prop="address" label="地址"></el-table-column> -->
+        </el-table>
         </div>
 
-        <div>
-            <button @click="backToChoose()">返回上一级</button>
+
+        <div class="two-button">
+            <!-- <button @click="submitButton()">提交</button> -->
+            <el-button type="primary" @click="submitButton()">提交</el-button>
+            <el-button type="primary"  @click="backToChoose()">返回上一级</el-button>
         </div>
+
+        <!-- <div> -->
+            <!-- <button @click="backToChoose()">返回上一级</button> -->
+            
+        <!-- </div> -->
         <!-- 展示从后端搜索出来的数据 -->
         
+        <hr class="new1">
         <div class="ct">
             <el-checkbox :indeterminate="isIndeterminate" v-model="checkAll" @change="handleCheckAllChange">全选</el-checkbox>
             <div style="margin: 15px 0;"></div> 
             <el-checkbox-group v-model="checkedFlies" @change="handleCheckedChange">
-                <div v-for="file in fileSearchList"  :key="file.id">
+                <div v-for="file in fileSearchList"  :key="file.id" class="show-checkbox">
                     <el-checkbox  :label="file.id">{{ file.filename }}</el-checkbox>
-                    <div v-for="data in file.content" :key="data.line">
-                        <span>{{ data.line }}</span>
-                        <span>{{ data.text }}</span>
+                    <hr class="new1">
+                    <div v-for="data in file.text" :key="data.line">
+                        <span class="sep">行号：{{ data.line }}</span>
+                        <!-- <span>&nbsp &nbsp &nbsp</span> -->
+                        <span class="sep">{{ data.content }}</span>
                     </div>
+                    <!-- <hr class="new1"> -->
+                    <!-- <div> </div> -->
                 </div>
                 
             </el-checkbox-group> 
-        </div> 
+        </div>
+        
+        <div>
+            <!-- <button @click="download()">下载</button> -->
+            <el-button type="primary"  @click="download()">下载</el-button>
+        </div>
         
     </div>
 </template>
@@ -39,6 +66,7 @@
     import axiox from "axios"
     var id = 0;
     var currentArray = []
+    var searchback = []
     export default {
         created:function(){
             // 调用接口获取服务器的盘符
@@ -74,34 +102,28 @@
                 tableData: [],
                 input: '',
                 searchkey:"",
-                fileSearchList: [{
-                        id:1,
-                        filename:'zhy',
-                        content:[
-                            {line:"1",text:"123"},
-                            {line:"2",text:"123"},
-                            {line:"3",text:"123"},
-                    ]},
-                    {
-                        id:2,
-                        filename:'zhy',
-                        content:[
-                            {line:"1",text:"123"},
-                            {line:"2",text:"123"},
-                            {line:"3",text:"123"},
-                    ]}
-                ],
+                fileSearchList: [],
+                // [{
+                //         id:1,
+                //         filename:'zhy',
+                //         text:[
+                //             {line:"1",content:"123"},
+                //             {line:"2",content:"123"},
+                //             {line:"3",content:"123"},
+                //     ]},
+                //     {
+                //         id:2,
+                //         filename:'zhy',
+                //         text:[
+                //             {line:"1",content:"123"},
+                //             {line:"2",content:"123"},
+                //             {line:"3",content:"123"},
+                //     ]}
+                // ],
                 choose: "", 
             }
         },
         methods:{
-            getUrlFile:function(){
-                // 获取 input 中的 url 内地址
-                // 提交url地址，像服务端获取 JSON 数据来渲染页面
-                console.log("我触发了点击事件");
-                // 更新渲染表格
-                // 并展示
-            },
             tableButtonClick:function(){
                 // table中的button的点击事件
                 // 将路径更新input
@@ -150,16 +172,16 @@
                     const response = await axiox.get("http://localhost:8888/getDir?dir="+input)
                     currentArray = response.data
 
-                    console.log("currentArray")
-                    console.log(currentArray)
+                    // console.log("currentArray")
+                    // console.log(currentArray)
                     let td = []
                     for(let i=0;i<currentArray.length;i++){
                         let a = {}
                         a.path = currentArray[i]
                         td.push(a)
                     }
-                    console.log(td)
-                    console.log(this.tableData)
+                    // console.log(td)
+                    // console.log(this.tableData)
                     this.tableData = td
                 }
                 catch(error){
@@ -167,15 +189,31 @@
                 }
             },
             submitButton:async function(){
-                try{
-                    // 等待后端的返回的端口已经方式
-                    const response = await axiox.get("http://localhost:8888/getDir?dir="+this.input+"&&key="+this.searchkey)
-                    currentArray = response.data
-                    // 从后端获取返回的数据
-                    
+                console.log(this.searchkey)
+                console.log(this.input)
+                if(this.searchkey.trim()===""){
+                    alert("请输入关键词")
                 }
-                catch(error){
-                    console.error(error) 
+                else{
+                    try{
+                        // 等待后端的返回的端口已经方式
+                        const response = await axiox.get("http://localhost:8888/fileSearch?dir="+this.input+"&&key="+this.searchkey)
+                        searchback = response.data
+                        // 从后端获取返回的数据
+                        // 将返回的数据编程为需要的格式 赋值 
+                        console.log(searchback)
+                        let a = new Array()
+                        for (let key in searchback) {
+                            console.log(key) // foo, bar
+                            console.log(searchback[key])
+                            
+                            a.push(this.analysisKeyValue(key,searchback[key]))
+                        }
+                        this.fileSearchList = a
+                    }
+                    catch(error){
+                        console.error(error) 
+                    }
                 }
             },
             backToChoose: async function(){
@@ -248,6 +286,40 @@
                 let checkedCount = value.length;
                 this.checkAll = checkedCount === this.fileSearchList.length;
                 this.isIndeterminate = checkedCount > 0 && checkedCount < this.fileSearchList.length;
+            },
+            download:async function(){
+                // 1、汇总所有选中的id
+                let a = ""
+                for(let i=0;i<this.checkedFlies.length-1;i++){
+                    a+=this.checkedFlies[i]+"-"
+                }
+                a += this.checkedFlies[this.checkedFlies.length-1]
+                // console.log(a)
+                try{
+                    // 等待后端的返回的端口已经方式
+                    const response = await axiox.get("http://localhost:8888/download?key="+this.searchkey+"&ids="+a)
+                    // searchback = response.data
+                    }
+                    catch(error){
+                        console.error(error) 
+                    }
+
+
+
+
+            },
+            analysisKeyValue:function(key,value){
+                // id=19, dir='C:/Users/Admin/Desktop//计算机志愿.docx'
+                    let arr = []
+                    arr = key.split(",")
+                    for(let i=0;i<arr.length;i++){
+                        arr[i] = arr[i].slice(arr[i].indexOf("=")+1)
+                    }
+                    let filecontext = new Object();
+                    filecontext.id = parseInt(arr[0])
+                    filecontext.filename = arr[1]
+                    filecontext.text = value
+                    return filecontext
             }
         }
     }
@@ -264,18 +336,48 @@
         margin: auto;
         margin-bottom: 10px;
     }
-    /* .showlist{
-        font-size: 10;
-    } */
-    /* .el-checkbox-group {
-        font-size:10
-    } */
+
+    .two-button{
+        margin: 20px;
+    }
+    
+    hr.new1 {
+        border-top: 1px solid rgba(222, 222, 222,0.3);
+    }
+
+    .file-choose-table {
+        /* text-align: center; */
+        display: flex;
+        justify-content:center;
+    }
+
+    .sep {
+        margin-left: 10px;
+        margin-right: 10px;
+    }
+
+    .show-checkbox {
+        margin-bottom: 20px;
+
+    }
+    
 </style>
 
 <style scoped>
     .ct >>> .el-checkbox-group {
-        font-size:20px 
+        font-size:15px 
     }
+    .file-choose-table >>> .el-table .el-table__cell{
+        /* margin: auto; */
+        text-align: center;
+    }
+    .file-choose-table >>> .el-table__body-wrapper {
+        height: 200px; /* 滚动条整体高 必须项 */
+        border-right: none;
+        overflow-y: scroll;/* overflow-y为了不出现水平滚动条*/
+    }
+    
+    
 </style>
 
 
